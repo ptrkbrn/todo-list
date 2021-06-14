5075cd7b6f6912ac5f85fe0e9641ba8bdaca2fbc
Patrick Breen <pressure78@gmail.com>
2021-06-14 13:19:49 -0400

    Updates

---
+++ precache-manifest.e2c10f5f0a40a8f5fa8bb99da39eabc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "070529849370bc1851221e088f82fc62",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3e2239871bae4d1e054a",
+    "url": "/todo-list/static/css/main.ca3980d7.chunk.css"
+  },
+  {
+    "revision": "098b73bd2ea037ccee6e",
+    "url": "/todo-list/static/js/2.3bbb8ebe.chunk.js"
+  },
+  {
+    "revision": "3e2239871bae4d1e054a",
+    "url": "/todo-list/static/js/main.779a1e86.chunk.js"
+  },
+  {
+    "revision": "da715ee0b4f01398cebe",
+    "url": "/todo-list/static/js/runtime-main.14b3a6b5.js"
+  }
+]);
